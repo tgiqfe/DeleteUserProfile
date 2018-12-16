93d96ae903da3cc73dd41e3bfba48d0708c5b893
tgiqfe <tqji31001@gmail.com>
2018-12-16 15:54:32 +0900

    初回同期

---
+++ DeleteUserProfile/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Management;
+
+namespace DeleteUserProfile
+{
+    class Program
+    {
+        //  静的パラメータ
+        const string KEY_PROFILE_LIST = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList";
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                //  削除対象のユーザー名
+                string targetUser = args[0];
+
+                //  現在のプロファイルフォルダーリストを確認
+                List<UserInfo> userInfoList = new List<UserInfo>();
+                using (RegistryKey regKey = Registry.LocalMachine.OpenSubKey(KEY_PROFILE_LIST))
+                {
+                    foreach (string sid in regKey.GetSubKeyNames())
+                    {
+                        UserInfo ui = new UserInfo(sid);
+                        if (ui.IsEnable())
+                        {
+                            userInfoList.Add(ui);
+                        }
+                    }
+                }
+
+                //  UserInfoリストから削除対象ユーザーを探す
+                if (userInfoList.Count > 0)
+                {
+                    UserInfo matchUI =
+                        userInfoList.FirstOrDefault(x =>
+                            x.Name.Equals(targetUser, StringComparison.OrdinalIgnoreCase) ||
+                            x.UserName.Equals(targetUser, StringComparison.OrdinalIgnoreCase));
+                    if (matchUI != null)
+                    {
+                        Console.WriteLine("削除対象：" + matchUI.Name);
+                        Console.Write("削除します。よろしいですか? (y/N)");
+                        string result = Console.ReadLine();
+                        if (result.ToLower() == "y")
+                        {
+                            //  削除実行
+                            matchUI.MO_Profile.Delete();
+                        }
+                        else
+                        {
+                            Console.WriteLine("削除中止");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("削除対象のプロファイルデータ無し");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("削除対象のプロファイルデータ無し");
+                }
+            }
+        }
+    }
+}
---
+++ DeleteUserProfile/UserInfo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.IO;
+
+namespace DeleteUserProfile
+{
+    class UserInfo
+    {
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string DomainName { get; set; }
+        public string ProfileDir { get; set; }
+        public string SID { get; set; }
+        public ManagementObject MO_Profile { get; set; }
+        public ManagementObject MO_Account { get; set; }
+
+        public UserInfo() { }
+        public UserInfo(string sid)
+        {
+            this.SID = sid;
+            MO_Account = new ManagementClass("Win32_UserAccount").
+                GetInstances().
+                OfType<ManagementObject>().
+                FirstOrDefault(x => x["SID"].ToString() == sid);
+            MO_Profile = new ManagementClass("Win32_UserProfile").
+                GetInstances().
+                OfType<ManagementObject>().
+                FirstOrDefault(x => x["SID"].ToString() == sid);
+            if (MO_Account != null)
+            {
+                this.Name = MO_Account["Domain"].ToString() + "\\" + MO_Account["Name"].ToString();
+                this.DomainName = MO_Account["Domain"].ToString();
+                this.UserName = MO_Account["Name"].ToString();
+            }
+            if (MO_Profile != null)
+            {
+                this.ProfileDir = MO_Profile["LocalPath"].ToString();
+            }
+        }
+
+        public bool IsEnable()
+        {
+            return 
+                Name != null &&
+                DomainName != null &&
+                UserName != null &&
+                ProfileDir != null &&
+                Directory.Exists(ProfileDir);
+        }
+    }
+}
